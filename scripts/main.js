d7d270b4cf69d080b9b9e07b6cad8eb4685c2847
Selena Groh <selena.groh@gmail.com>
2018-04-25 16:03:33 -0400

    layout: map next to info, added button & battery formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,138 +1,145 @@
 var url = "localhost:9090";
 var connect = false;
-var cmd;
+var ip;
+//var cmd;
 
-function updateIP(){
-		url = document.getElementById("ip").value;
-		ip = "ws://" + url;
-		connect = true;
-		init(); 
+function init() {
+  'use strict';
+  var ros, keyboard, viewer, nav;
+  
+  if (connect) {
+
+    // initiate ros object
+    ros = new ROSLIB.Ros({
+      url: ip
+    });
+
+    // handle exceptions
+    ros.on('connection', function () {
+      var status = document.getElementById("status");
+      status.innerHTML = "<p id='status' style='color:green;'><strong>STATUS: CONNECTED</strong></p>";
+    });
+
+    ros.on('error', function (error) {
+      var status = document.getElementById("status");
+      status.innerHTML = "<p id='status' style='color:red;'><strong>ERROR: FAILED TO CONNECT</strong></p>";
+    });
+
+    ros.on('close', function () {
+      var status = document.getElementById("status");
+      status.innerHTML = "<p id='status' style='color:red;'><strong>STATUS: DISCONNECTED</strong></p>";
+    });
+
+    // initiate keyboardteleop object
+    keyboard = new KEYBOARDTELEOP.Teleop({
+      ros: ros,
+      topic: '/cmd_vel_mux/input/teleop'
+    });
+
+    // update the speed      
+    keyboard.scale = 0.4;
+
+    // hide the "no map loaded" message
+    document.getElementById('no-map-loaded').style.display = 'none';
+
+    // create a new map object
+    // Create the main viewer.
+    viewer = new ROS2D.Viewer({
+      divID : 'map',
+      width : 720,
+      height : 720
+    });
+
+    // Setup the nav client
+    nav = NAV2D.OccupancyGridClientNav({
+      ros : ros,
+      rootObject : viewer.scene,
+      viewer : viewer,
+      serverName : '/move_base'
+    });
+
+//		// publish cmd topic to the turtlebot
+//	    cmd = new ROSLIB.Topic({
+//			ros: ros,
+//			name: '/cmd_vel_mux/input/teleop',
+//			messageType: 'geometry_msgs/Twist'
+//		});
+  }
 }
 
-function init(){
-
-	if (connect){
-
-		// initiate ros object
-		ros = new ROSLIB.Ros({
-			url: ip
-		});
-
-		// handle exceptions
-		ros.on('connection', function(){
-			var status = document.getElementById("status");
-			status.innerHTML = "<p id='status' style='color:green;'><strong>STATUS: CONNECTED</strong></p>";
-		});
-
-		ros.on('error', function(error){
-			var status = document.getElementById("status");
-			status.innerHTML = "<p id='status' style='color:red;'><strong>ERROR: FAILED TO CONNECT</strong></p>";
-		});
-      
-        ros.on('close', function(){
-            var status = document.getElementById("status");
-			status.innerHTML = "<p id='status' style='color:red;'><strong>STATUS: DISCONNECTED</strong></p>";
-        })
-
-		// initiate keyboardteleop object
-		keyboard = new KEYBOARDTELEOP.Teleop({
-			ros: ros,
-			topic: '/cmd_vel_mux/input/teleop'
-		});
-
-		// update the speed      
-        keyboard.scale = 0.4;
-
-        // create a new map object
-         // Create the main viewer.
-	    var viewer = new ROS2D.Viewer({
-	      divID : 'map',
-	      width : 720,
-	      height : 720
-	    });
-
-	    // Setup the nav client
-	    var nav = NAV2D.OccupancyGridClientNav({
-	      ros : ros,
-	      rootObject : viewer.scene,
-	      viewer : viewer,
-	      serverName : '/move_base'
-	    });
-
-		// publish cmd topic to the turtlebot
-	    cmd = new ROSLIB.Topic({
-			ros: ros,
-			name: '/cmd_vel_mux/input/teleop',
-			messageType: 'geometry_msgs/Twist'
-		});
-	}
+function updateIP() {
+  'use strict';
+  
+  url = document.getElementById("ip").value;
+  ip = "ws://" + url;
+  connect = true;
+  init();
 }
 
-// move turtlebot forward
-function forward(){
-	var twist = new ROSLIB.Message({
-		linear: {
-			x: 0,
-			y: 0,
-			z: 0 
-		}, 
-		angular: {
-			x: 0,
-			y: 0, 
-			z: 0
-		}
-	});
-	cmd.publish(twist);
-}
-
-// move turtlebot backward
-function backward(){
-	var twist = new ROSLIB.Message({
-		linear: {
-			x: 0,
-			y: 0,
-			z: 0 
-		}, 
-		angular: {
-			x: 0,
-			y: 0, 
-			z: 0
-		}
-	});
-	cmd.publish(twist);
-}
-
-// move turtlebot left
-function left(){
-	var twist = new ROSLIB.Message({
-		linear: {
-			x: 0,
-			y: 0,
-			z: 0 
-		}, 
-		angular: {
-			x: 0,
-			y: 0, 
-			z: 0
-		}
-	});
-	cmd.publish(twist);
-}
-
-// move turtlebot right
-function right(){
-	var twist = new ROSLIB.Message({
-		linear: {
-			x: 0,
-			y: 0,
-			z: 0 
-		}, 
-		angular: {
-			x: 0,
-			y: 0, 
-			z: 0
-		}
-	});
-	cmd.publish(twist);
-}
-
+//// move turtlebot forward
+//function forward(){
+//	var twist = new ROSLIB.Message({
+//		linear: {
+//			x: 0,
+//			y: 0,
+//			z: 0 
+//		}, 
+//		angular: {
+//			x: 0,
+//			y: 0, 
+//			z: 0
+//		}
+//	});
+//	cmd.publish(twist);
+//}
+//
+//// move turtlebot backward
+//function backward(){
+//	var twist = new ROSLIB.Message({
+//		linear: {
+//			x: 0,
+//			y: 0,
+//			z: 0 
+//		}, 
+//		angular: {
+//			x: 0,
+//			y: 0, 
+//			z: 0
+//		}
+//	});
+//	cmd.publish(twist);
+//}
+//
+//// move turtlebot left
+//function left(){
+//	var twist = new ROSLIB.Message({
+//		linear: {
+//			x: 0,
+//			y: 0,
+//			z: 0 
+//		}, 
+//		angular: {
+//			x: 0,
+//			y: 0, 
+//			z: 0
+//		}
+//	});
+//	cmd.publish(twist);
+//}
+//
+//// move turtlebot right
+//function right(){
+//	var twist = new ROSLIB.Message({
+//		linear: {
+//			x: 0,
+//			y: 0,
+//			z: 0 
+//		}, 
+//		angular: {
+//			x: 0,
+//			y: 0, 
+//			z: 0
+//		}
+//	});
+//	cmd.publish(twist);
+//}
